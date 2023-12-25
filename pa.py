e42bb06d0d6772a6bbb1a6ca56d4894ef8b9e4bd
Elik <elik.zakai@gmail.com>
2023-12-25 21:50:55 +0200

    added folders

---
+++ pa.py
@@ -0,0 +1,4 @@
+password = "veHt1JK5X"
+clusterName = "cluster0.qnr3p"
+user_name = "qa_agency"
+db_name = "trado_qa"
---
+++ src/utils/webdriver_set_up.py
@@ -0,0 +1,37 @@
+import time
+import unittest
+
+from src.utils.imports import *
+
+
+class WebDriverSetup(unittest.TestCase):
+    def setUp(self) -> None:
+        try:
+            # setUp the driver
+
+            # options = webdriver.ChromeOptions()
+
+            chrome_options = Options()
+            chrome_options.add_experimental_option("detach", True)
+            chrome_options.add_argument("--ignore-ssl-errors=yes")
+            chrome_options.add_argument("--start-maximized")
+            chrome_options.add_argument("--ignore-certificate-errors")
+            chrome_options.add_argument("disable_extensions")
+            chrome_options.headless = False
+            self.driver = webdriver.Chrome(
+                service=ChromeService(ChromeDriverManager().install())
+            )
+            self.driver.set_page_load_timeout(30)
+            self.driver.get("http://qa.trado.ai/")
+
+            time.sleep(5)
+
+        except AssertionError:
+            self.driver.quit()
+
+    def tearDown(self) -> None:
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
